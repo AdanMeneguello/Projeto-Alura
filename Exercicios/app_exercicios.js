11f605fbdddff75f33663f92a543350b564d8067
Adan Meneguello <meneguelloadan@gmail.com>
2024-05-24 18:00:31 -0300

    Adcionando mini calculadora na pasta exercicios

--- Exercicios/app_exercicios.js
+++ Exercicios/app_exercicios.js
@@ -1,7 +1,41 @@
-function capturarValores(){
-   let nome = document.getElementById('campoNome').value;
-   let idade = document.getElementById('campoIdade').value;
-   
-   let mostraNome = document.getElementById('mostraNome'); 
-   mostraNome.innerHTML = `<span>Nome: ${nome} - Idade: ${idade}</span>`
+function Calculo(){
+   let input1 = parseFloat(document.getElementById('campoInput1').value);
+   let operador = document.getElementById('operador').value;
+   let input2 = parseFloat(document.getElementById('campoInput2').value);
+
+   if(operador == "+"){
+      document.getElementById('Resultado').innerHTML = `<span>Resultado:${operadorAdição(input1, input2)} </span>`  
+   }
+   else if (operador == "-"){
+      document.getElementById('Resultado').innerHTML = `<span>Resultado:${operadorSubtração(input1, input2)} </span>`;
+   }
+   else if (operador == "x"){
+      document.getElementById('Resultado').innerHTML = `<span>Resultado:${operadorMultiplicação(input1, input2)} </span>`;
+   }
+   else if(operador == "/"){
+      document.getElementById('Resultado').innerHTML = `<span>Resultado:${operadorDivisão(input1, input2)} </span>`;
+   }
+   else if(operador == "*"){
+      document.getElementById('Resultado').innerHTML = `<span>Resultado:${operadorExponenciação(input1, input2)} </span>`;
+   }
+}
+
+function operadorAdição(a,b){
+   return a + b;
+}
+
+function operadorSubtração(a,b){
+   return a - b;
+}
+
+function operadorMultiplicação(a,b){
+   return a * b;
+}
+
+function operadorDivisão(a,b){
+   return a / b;
+}
+
+function operadorExponenciação(a,b){
+   return a ** b;
 }
