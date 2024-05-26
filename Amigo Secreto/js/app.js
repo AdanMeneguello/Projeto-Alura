5229895e6b279a16bfc97262f2c71668b0b0301c
Adan Meneguello <meneguelloadan@gmail.com>
2024-05-26 18:00:29 -0300

    Amigo secreto js botao adcionar e sortear feito

---
+++ Amigo Secreto/js/app.js
@@ -0,0 +1,54 @@
+let listaNomes = [];
+while(listaNomes.lenght){
+    adicionar();
+    if(sortear()){
+        break;
+    }
+}
+
+function adicionar(){
+    let inputNomeAmigo = document.getElementById('nome-amigo').value;
+    listaNomes.push(inputNomeAmigo);
+
+    inputNomeAmigo.innerHTML = `<input id="nome-amigo" class="form__input" type="text" placeholder="Nome do amigo">`
+    let caixaListaAmigos = document.getElementById('lista-amigos');
+    caixaListaAmigos.innerHTML = ` <p id="lista-amigos">${listaNomes}</p> `
+}
+   
+function sortear(){
+    alert("Sorteou");
+    let indiceSorteado_ListaNomes;
+    let listaNomesSorteados = [];
+    let quantidade = listaNomes.length;
+
+    for(let i = 0; i < quantidade; i++){
+        indiceSorteado_ListaNomes = listaNomes[ObterNumeroAleatorio(0, quantidade)]
+        while(listaNomesSorteados.includes(indiceSorteado_ListaNomes)){
+            indiceSorteado_ListaNomes = listaNomes[ObterNumeroAleatorio(0, quantidade)];
+        }
+        listaNomesSorteados.push(indiceSorteado_ListaNomes)
+    }
+    alert(listaNomesSorteados);  
+    
+    let index = 0;
+    
+    let resultadoSorteio = document.getElementById('lista-sorteio');
+    let linhaSorteio;
+
+    let listaSorteadosFinal = [];
+    for(let y = 0; y < listaNomesSorteados.length - 1; y++){
+        linhaSorteio = `<p id="lista-sorteio">${listaNomesSorteados[index]} --> ${listaNomesSorteados[index + 1]}</p>`;
+        listaSorteadosFinal.push(linhaSorteio)
+        alert(listaSorteadosFinal);
+        index++
+    }
+
+    listaSorteadosFinal.push(`<p id="lista-sorteio">${listaNomesSorteados[listaNomesSorteados.length - 1]} --> ${listaNomesSorteados[listaNomesSorteados.length - listaNomesSorteados.length]}</p>`);
+    resultadoSorteio.innerHTML = listaSorteadosFinal.join('');                                                           
+}
+
+function ObterNumeroAleatorio (min, max){
+    return Math.floor(Math.random() * (max - min) - min);
+}
+
+
